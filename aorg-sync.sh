d2326f5d839374adf24492940c53bec3bf86d14c
xristos <xristos@sdf.org>
2024-04-26 20:14:46 -0400

    Initial commit

---
+++ aorg-sync.sh
@@ -0,0 +1,333 @@
+#!/usr/bin/env bash
+#
+# This script will synchronize an archive.org 'item' with a local directory.
+# Only new files and files that have changed are downloaded. These checks take
+# place based on file size, mtime and md5 hash.
+#
+# Requirements:
+#
+#   curl, jq, md5sum, GNU coreutils/sed/grep
+#
+# This is free and unencumbered software released into the public domain.
+# xristos@sdf.org
+
+set -e
+
+#
+# Configuration
+#
+
+: "${BASE:=https://archive.org}"
+
+# archive.org identifier for the item e.g.
+# https://archive.org/details/[identifier]
+: "${REMOTE:=0mhz-dos}"
+
+# Change these to GNU on macOS as BSD versions will not do
+: "${STAT:=stat}"
+: "${TOUCH:=touch}"
+
+#
+# End of user configuration
+#
+
+CURL_ARGS=(-RSLf#)
+
+index="${BASE}/download/${REMOTE}/${REMOTE}_files.xml"
+index_md5=""
+
+# HTTP metadata response (JSON)
+md_json=""
+# Multiline output: md5 size mtime name
+md_parsed=""
+# Multiline output: name
+md_removed=""
+
+# CLI options
+do_index=1
+do_prompt=1
+do_force=0
+do_delete=0
+skip_mtime=1
+force_mtime=1
+
+# Stats
+new=0
+updated=0
+downloaded=0
+deleted=0
+failed=0
+hashed=0
+
+exitcode=0
+
+function usage {
+  echo "Usage: $0 [--prompt] [--force] [--delete] [--no-mtime] [--no-force-mtime]"
+  echo -e "  --force           force processing even if index hasn't changed"
+  echo -e "                    ignored if --no-index is used"
+  echo -e "  --delete          delete files that have been removed from the index"
+  echo -e "                    WARN: This will delete all files present in the directory,"
+  echo -e "                    that do not exist in the index"
+  echo
+  echo -e "  --no-index        do not download/process an index file (${REMOTE}_files.xml)"
+  echo -e "  --no-prompt       do not prompt if index hasn't changed"
+  echo -e "                    if --force, continue, otherwise exit"
+  echo -e "  --no-mtime        do not check mtime to skip checking files whose size hasn't changed,"
+  echo -e "                    hash every file"
+  echo -e "  --no-force-mtime  do not auto-update mtime for every processed file"
+  echo
+}
+
+function fetch_metadata {
+  printf '[-] Fetching %s\n' "${BASE}/metadata/${REMOTE}"
+  if ! md_json="$(curl "${CURL_ARGS[@]}" "${BASE}/metadata/${REMOTE}")"; then
+    return 1
+  fi
+  if ! md_parsed="$(jq -r '.files | .[] | "\(.md5) \(.size) \(.mtime) \(.name)"' \
+                      <<< "$md_json")"; then
+     return 1
+  fi
+  if ! index_md5="$(grep -E " ${REMOTE}_files.xml$" <<< "$md_parsed" | awk '{print $1}')"; then
+    printf '[!] Missing %s_files.xml" in metadata JSON response\n' "${REMOTE}"
+    return 1
+  fi
+}
+
+function fetch_index {
+  if [ "$do_index" -eq 0 ]; then
+    return 0
+  fi
+  # Check local index if it exists
+  if [ -e "${REMOTE}_files.xml" ]; then
+    # Hash it to see if it has changed
+    if check_hash "${REMOTE}_files.xml" "$index_md5"; then
+      if [ "$do_prompt" -eq 1 ]; then
+        read -n1 -r -p "[-] No changes to index, continue (y/n)? " reply
+        echo
+        if [ "$reply" = "n" ]; then
+          exit 0
+        fi
+      else
+        if [ "$do_force" -eq 0 ]; then
+          printf '[*] No changes to index\n'
+          exit 0
+        fi
+        printf '[*] No changes to index, forcing update\n'
+      fi
+    else
+      printf '[!] Index has changed -> %s\n' "$index_md5"
+    fi
+  else
+    printf '[!] Local index does not exist\n'
+  fi
+  printf '[+] Downloading %s to %s\n' "$index" "${REMOTE}_files.xml"
+  if ! curl "${CURL_ARGS[@]}" -o "${REMOTE}_files.xml" "$index"; then
+   return 1
+  fi
+  if ! check_hash "${REMOTE}_files.xml" "$index_md5"; then
+    printf '[!] Failed MD5 check, expected: %s\n' "$index_md5"
+  fi
+}
+
+function check_hash {
+  local file md5
+  file="$1"
+  md5="$2"
+
+  if ! lmd5="$(md5sum "$file" | awk '{print $1}')"; then
+    return 1
+  fi
+  if [ "$lmd5" != "$md5" ]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+function check_downloaded_file {
+  local file md5 size lsize mtime
+  file="$1"
+  md5="$2"
+  size="$3"
+  mtime="$4"
+
+  lsize="$(du -b "${file}.new" | awk '{print $1}')"
+  if [ "$lsize" = "$size" ]; then
+    # Check hash
+    if ! check_hash "${file}.new" "$md5"; then
+      exitcode=1
+      ((failed++)) || true
+      printf '[!!] MD5 FAIL\n'
+    else
+      mv "${file}.new" "$file"
+      if [ "$force_mtime" -eq 1 ]; then
+        # Curl should preserve mtime, but set it regardless if force_mtime is set
+        "$TOUCH" --date="@$mtime" "$file"
+      fi
+    fi
+  else
+    exitcode=1
+    ((failed++)) || true
+    printf '[!!] size %s != %s\n' "$lsize" "$size"
+  fi
+  return 0
+}
+
+function download_file {
+  local file encoded
+  file="$1"
+  if ! encoded="$(printf '%s' "$1" | jq -sRr @uri)"; then
+    ((failed++)) || true
+    return 1
+  fi
+  if curl "${CURL_ARGS[@]}" -o "${file}.new" "${BASE}/download/${REMOTE}/${encoded}"; then
+    ((downloaded++)) || true
+    return 0
+  else
+    ((failed++)) || true
+    return 1
+  fi
+}
+
+function diff_files {
+  while read -r md5 size mtime file ; do
+    if [ "$file" = "${REMOTE}_files.xml" ]; then
+      # Skip the index as it's been previously processed
+      continue
+    fi
+    if [ ! -f "$file" ]; then
+      # Does not exist
+      printf '[++] %s\n' "$file"
+      download_file "$file" || continue
+      check_downloaded_file "$file" "$md5" "$size" "$mtime"
+      ((new++)) || true
+    else
+      local lsize
+      lsize="$(du -b "$file" | awk '{print $1}')"
+      if [ "$lsize" != "$size" ]; then
+        # Exists but size differs
+        printf '[+s] %s\n' "$file"
+        download_file "$file" || continue
+        check_downloaded_file "$file" "$md5" "$size" "$mtime"
+        ((updated++)) || true
+      else
+        # Exists but size is identical
+        if [ "$skip_mtime" -eq 1 ]; then
+          # Skip processing if mtime is identical
+          local lmtime
+          lmtime="$("$STAT" -c %Y "$file")"
+          if [ "$lmtime" = "$mtime" ]; then
+            printf '[..] %s\n' "$file"
+            continue
+          fi
+        fi
+        printf '[==] %s\n' "$file"
+        ((hashed++)) || true
+        if ! check_hash "$file" "$md5"; then
+          download_file "$file" || continue
+          check_downloaded_file "$file" "$md5" "$size" "$mtime"
+          ((updated++)) || true
+        else
+          # File is OK, force update mtime if set
+          if [ "$force_mtime" -eq 1 ]; then
+            "$TOUCH" --date="@$mtime" "$file"
+          fi
+        fi
+      fi
+    fi
+  done <<< "$md_parsed"
+  md_removed="$(comm -13 \
+                  <(cut -d' ' -f4- <<< "$md_parsed" | sort) \
+                  <(for f in *; do echo "$f"; done | grep -Ev '(.xml$|.sh$)' | sort))"
+  if [ -n "$md_removed" ]; then
+    while read -r file ; do
+      printf '[##] %s\n' "$file"
+      if [ "$do_delete" -eq 1 ]; then
+        rm -f "$file"
+        ((deleted++)) || true
+      fi
+    done <<< "$md_removed"
+  fi
+  printf '[-]\n'
+}
+
+function print_stats {
+  local total
+  total="$(wc -l <<< "$md_parsed")"
+  printf '[*] Total:\t %d\n' "$total"
+  printf '[*] Downloaded:\t %d\n' "$downloaded"
+  printf '[*] New:\t %d\n' "$new"
+  printf '[*] Updated:\t %d\n' "$updated"
+  printf '[*] Failed:\t %d\n' "$failed"
+  printf '[*] Hashed:\t %d\n' "$hashed"
+  printf '[*] Deleted:\t %d\n' "$deleted"
+}
+
+function interrupt {
+  trap - INT
+  echo
+  print_stats
+  exit 1
+}
+
+
+#
+# Argument parsing
+#
+
+
+while :; do
+  case "$1" in
+    -h|--help)
+      usage
+      exit 0
+      ;;
+    --prompt)
+      do_prompt=1
+      ;;
+    --force)
+      do_force=1
+      ;;
+    --delete)
+      do_delete=1
+      ;;
+    --no-mtime)
+      skip_mtime=0
+      ;;
+    --no-force-mtime)
+      force_mtime=0
+      ;;
+    --)
+      shift
+      break
+      ;;
+    -?*)
+      echo "Unknown option: $1" >&2
+      exit 1
+      ;;
+    *)
+      break
+  esac
+  shift
+done
+
+
+#
+# End of argument parsing
+#
+
+
+fetch_metadata
+fetch_index
+
+trap interrupt INT
+diff_files
+trap - INT
+
+print_stats
+
+if [ "$exitcode" -eq 0 ]; then
+  exit 0
+fi
+
+exit 1
